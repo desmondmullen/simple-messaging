4303581b11303aa87043b445b60c684b9d8aaaf4
Desmond Mullen <dsm@busylittlestudios.com>
2019-01-30 14:58:00 -0500

    v1.57

--- logic.js
+++ logic.js
@@ -11,6 +11,15 @@ $(document).ready(function () {
     firebase.initializeApp(config);
 
     var database = firebase.database();
+    var actionCodeSettings = {
+        // URL you want to redirect back to. The domain (www.example.com) for this URL
+        // must be whitelisted in the Firebase Console.
+        'url': 'https://desmondmullen.com/simple-messaging/', // Here we redirect back to this same page.
+        // 'url': window.location.href, // Here we redirect back to this same page.
+        'handleCodeInApp': true // This must be true.
+    };
+
+
     var userID;
     var userSignedIn;
     var userEmail;
@@ -53,6 +62,13 @@ $(document).ready(function () {
                 ", Longitude: " + userLongitude
         });
         emptyInputFields();
+    });
+
+    $("#send-link").on("click", function () {
+        let theEmailAddressToSendLinkTo = prompt("Please enter the email address to send the link to:");
+        if (theEmailAddressToSendLinkTo != null) {
+            sendEmailLink(theEmailAddressToSendLinkTo);
+        }
     });
 
     database.ref(userMessagesPath).on("value", function (snapshot) {
@@ -142,6 +158,29 @@ $(document).ready(function () {
             }
             console.log(error);
         });
+    }
+
+    function sendEmailLink(theEmailAddress) {
+        firebase.auth().sendSignInLinkToEmail(theEmailAddress, actionCodeSettings).then(function () {
+            // Save the email locally so you don’t need to ask the user for it again if they open the link on the same device.
+            window.localStorage.setItem('emailForSignIn', email);
+            // The link was successfully sent. Inform the user.
+            alert('An email was sent to ' + email + '. Please use the link in the email to sign-in.');
+            // [START_EXCLUDE]
+            // Re-enable the sign-in button.
+            document.getElementById("sign-in").disabled = false;
+            // [END_EXCLUDE]
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            handleError(error);
+        });
+    }
+
+    function handleError(error) {
+        alert('Error: ' + error.message);
+        console.log(error);
+        document.getElementById("sign-in").disabled = false;
     }
 
     function sendPasswordReset() {
@@ -241,5 +280,5 @@ $(document).ready(function () {
     }
 
     //------------------------------------------------
-    console.log("v1.5");
+    console.log("v1.57");
 });
