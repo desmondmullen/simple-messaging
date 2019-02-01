3ba51dea762307ffe9c1c3e7db8ebe6530b06aac
Desmond Mullen <dsm@busylittlestudios.com>
2019-02-01 01:38:42 -0500

    user instance path

--- logic.js
+++ logic.js
@@ -161,24 +161,25 @@ $(document).ready(function () {
             // Disable the sign-in button during async sign-in tasks.
             // document.getElementById("sign-in").disabled = true;
             // Get the email if available.
-            var email = window.localStorage.getItem('emailForSignIn');
-            if (!email) {
-                // User opened the link on a different device. To prevent session fixation attacks, ask the
-                // user to provide the associated email again. For example:
-                email = window.prompt('Please provide the email you\'d like to sign-in with for confirmation.');
-            }
+            // var email = window.localStorage.getItem('emailForSignIn');
+            // if (!email) {
+            //     // User opened the link on a different device. To prevent session fixation attacks, ask the
+            //     // user to provide the associated email again. For example:
+            //     email = window.prompt('Please provide the email you\'d like to sign-in with for confirmation.');
+            // }
             if (email) {
                 firebase.auth().signInWithEmailLink(email, window.location.href).then(function (result) {
                     turnURLIntoUserInstancesPath();
                     // Clear the URL to remove the sign-in link parameters.
-                    if (history && history.replaceState) {
-                        window.history.replaceState({}, document.title, window.location.href.split('?')[0]);
-                    }
+                    // if (history && history.replaceState) {
+                    //     console.log("history");
+                    //     window.history.replaceState({}, document.title, window.location.href.split('?')[0]);
+                    // }
                     // Clear email from storage.
-                    window.localStorage.removeItem('emailForSignIn');
-                    var user = result.user;
-                    var isNewUser = result.additionalUserInfo.isNewUser;
-                    console.log(result)
+                    // window.localStorage.removeItem('emailForSignIn');
+                    // var user = result.user;
+                    // var isNewUser = result.additionalUserInfo.isNewUser;
+                    // console.log(result)
                 }).catch(function (error) {
                     handleError(error);
                 });
@@ -347,5 +348,5 @@ $(document).ready(function () {
 
 
     //------------------------------------------------
-    console.log("v1.7574");
+    console.log("v1.7575");
 });
